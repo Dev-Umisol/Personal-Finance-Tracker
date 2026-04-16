6ea9c0e4fbc578c75ccb69e16417fceb31c82b10
Dev-Umisol <148490822+Dev-Umisol@users.noreply.github.com>
2026-04-16 18:51:16 -0400

    Added type hunts to the functions and parameters, along with finished create_user, get_user_by_username, create_transaction, and get_all_transactions, still figuring out delete_transactions

--- app/crud.py
+++ app/crud.py
@@ -2,18 +2,36 @@ from sqlalchemy.orm import Session
 from pwdlib import PasswordHash
 from app import models
 
-def create_user(db: Session, username, password):
+# Create a new user with hashed password and store in the database
+def create_user(db: Session, user_name: str, password: str) -> models.Users:
     password_hash = PasswordHash.recommended()
     hashed_password = password_hash.hash(password)
+    
+    db_user = models.Users(user_name = user_name, user_password = hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    
+    return db_user
 
-def get_user_by_username(db: Session, username):
-    pass
+# Retrieve a user from the database by their username
+def get_user_by_username(db: Session, user_name: str) -> models.Users | None:
+    return db.query(models.Users).filter(models.Users.user_name == user_name).first()
 
-def create_transactions(db: Session, user_id, item_name, item_description, amount):
-    pass
+# Create a new transaction and store it in the database
+def create_transactions(db: Session, user_id: int, item_name: str, item_description: str, amount: float) -> models.Transactions:
+    db_transactions = models.Transactions(user_id = user_id, item_name = item_name, item_description = item_description, amount = amount)
+    
+    db.add(db_transactions)
+    db.commit()
+    db.refresh(db_transactions)
+    
+    return db_transactions
 
-def get_all_transactions(db: Session, user_id):
-    pass
+# Retrieve a transaction from the database by user_id
+def get_all_transactions(db: Session, user_id: int) -> models.Transactions | list:
+    return db.query(models.Transactions).filter(models.Transactions.user_id == user_id).all()
 
-def delete_transactions(db: Session, id):
+# Retrieve item id from the database and delete
+def delete_transactions(db: Session, id: int):
     pass
