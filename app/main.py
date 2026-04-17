6c035d192d40684878dada01bc3ad66364a61b7b
Dev-Umisol <148490822+Dev-Umisol@users.noreply.github.com>
2026-04-17 18:46:29 -0400

    fixed imports to what I actually need after planning the routes

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Depends, HTTPException
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from app import models, schemas, crud
-from app.main import app
-from app.database import get_db, Base
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+import jwt
+from datetime import datetime, timezone, timedelta
+from app import crud, schemas, models
+from app.database import get_db, engine
