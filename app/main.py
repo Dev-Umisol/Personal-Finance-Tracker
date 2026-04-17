ed60ef7e1982a15fcfd611a25c6c565c00cc0efe
Dev-Umisol <148490822+Dev-Umisol@users.noreply.github.com>
2026-04-17 18:47:04 -0400

    moved imports for better looking

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
-import jwt
 from datetime import datetime, timezone, timedelta
 from app import crud, schemas, models
 from app.database import get_db, engine
+import jwt
