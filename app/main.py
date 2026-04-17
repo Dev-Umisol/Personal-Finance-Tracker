aa240d235ec040d55cdefd45e1a3546598a197ce
Dev-Umisol <148490822+Dev-Umisol@users.noreply.github.com>
2026-04-17 18:08:25 -0400

    forgot a couple important imports lol

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from app import models, schemas, crud
 from app.main import app
 from app.database import get_db, Base
