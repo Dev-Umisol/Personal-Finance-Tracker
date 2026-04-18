a5d3d67274f81207db536b2b50e9d67a017419c3
Dev-Umisol <148490822+Dev-Umisol@users.noreply.github.com>
2026-04-18 13:45:55 -0400

    created create_access_token and updated gitignore for mac files added. create_access_token generates an access token for the payload and returns the payload, secret key, and algorithm

--- app/main.py
+++ app/main.py
@@ -4,4 +4,36 @@ from sqlalchemy.orm import Session
 from datetime import datetime, timezone, timedelta
 from app import crud, schemas, models
 from app.database import get_db, engine
+from dotenv import load_dotenv
+
 import jwt
+import os
+
+# Key Configuration
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+if SECRET_KEY is None:
+    raise ValueError("SECRET_KEY environment variable is not set.")
+
+app = FastAPI(title="Personal Finance Tracker")
+
+# Checks if table exists
+models.Base.metadata.create_all(bind=engine)
+
+# OAuth2 scheme for token-based authentication
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="users/login")
+
+# Generate a JWT access token for a logged-in user
+# includes subject, login action, and expiration time
+def create_access_token(user_name):
+    payload = {
+        "action": "login",
+        "sub": user_name,
+        "exp": datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    }
+
+    return jwt.encode(payload, SECRET_KEY, algorithm="HS256")
+
